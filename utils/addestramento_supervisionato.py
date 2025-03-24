2e693ca0ca8c2399c9391d4a1450c1f8724b3155
Linonia <d.gentile32@studenti.uniba.it>
2025-03-24 10:55:47 +0100

    Aggiunta random seed meglio

--- utils/addestramento_supervisionato.py
+++ utils/addestramento_supervisionato.py
@@ -288,7 +288,7 @@ def user_testing_sup_train(dataframe, stampe=False):
     # Generazione di un seed casuale per garantire ripetibilità nei test
     seed = time.time()   # random.randint(0, 10000)
     random.seed(seed)
-    np.random.seed(seed)
+    np.random.seed(random.random())
 
     print(f"Seed usato per il test: {seed}")
 
